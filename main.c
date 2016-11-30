e76ef600bf5d83e793e0cabe4ca8e93f2eb6f8c8
Osadebamwen Imade <DebaImade@Osadebamwens-MacBook-Pro.local>
2016-11-29 21:43:30 -0600

    Tokenized input from user

--- main.c
+++ main.c
@@ -1,42 +1,70 @@
+//#include <stdio.h>
+/* example one, to read a word at a time */
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
+#define MAXBUFFERSIZE   80
 
-#define BUFFER_SIZE 80
+void cleartoendofline( void );  /* ANSI function prototype */
+bool isValidCommand(char* userInput, char* command);
 
-char buffer[BUFFER_SIZE];
-char ch;
-int charCount;
-int exitFlag = 0;
-int validChoice;
-
-int int main(int argc, char const *argv[])
+void cleartoendofline( void )
 {
-	while(exitFlag == 0){
-	printf("$ ");
-	ch = getchar();
-	charCount = 0;
-	while((ch != "\n") && (charCount < BUFFER_SIZE)) {
-		buffer[charCount++] = ch;
-		ch = getchar();
-		putchar(buffer[charCount]);
-	}
-
-
-//	buffer[charCount] = 0x00;
-//	printf("%s ", buffer);
-
-	validChoice = 0;
-	while(validChoice == 0) {
-		/*
-
-
-
-			EXECUTE COMMANDS HERE
-
-
-		*/
-	}
-}
-	return 0;
+    char ch;
+    ch = getchar();
+    while( ch != '\n' )
+        ch = getchar();
 }
 
+/* Fucntion to check if entered command is valid */
+bool isValidCommand(char* userInput, char* command) {
+    return ()
+}
+
+int main()
+{
+    char    ch;                     /* handles user input */
+    char    buffer[MAXBUFFERSIZE];  /* sufficient to handle one line */
+    int     char_count;             /* number of characters read for this line */
+    int     exit_flag = 0;
+    int     valid_choice;
+    char*   token;
+    char*   tokens[3];
+
+    while( exit_flag  == 0 ) {
+        printf("Enter a line of text (<80 chars)\n");
+        ch = getchar();
+        char_count = 0;
+        while( (ch != '\n')  &&  (char_count < MAXBUFFERSIZE)) {
+            buffer[char_count++] = ch;
+            ch = getchar();
+        }
+        buffer[char_count] = 0x00;      /* null terminate buffer */
+//        printf("\nThe line you entered was:\n");
+//        printf("%s\n", buffer);
+
+        //tokenize user input
+        token = strtok(buffer, " ");
+        tokens[0] = token;
+
+        int tokenIndex = 1;
+        while(token != NULL) {
+            printf("token: %s\n", token);
+            token = strtok(NULL, " ");
+            tokens[tokenIndex++] = token;
+        }
+
+        valid_choice = 0;
+        while( valid_choice == 0 ) {
+            printf("Continue (Y/N)?\n");
+            scanf(" %c", &ch );
+            ch = toupper( ch );
+            if((ch == 'Y') || (ch == 'N') )
+                valid_choice = 1;
+            else
+                printf("\007Error: Invalid choice\n");
+            cleartoendofline();
+        }
+        if( ch == 'N' ) exit_flag = 1;
+    }
+}
